fa25cb2637e1b505747d89bfcdbc99b3e0d8763a
Amaan Khan <amaankhnak47@gmail.com>
2024-09-26 23:06:43 +0530

    e-commerce

---
+++ server/helpers/cloudinary.js
@@ -0,0 +1,24 @@
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: "",
+  api_key: "",
+  api_secret: "",
+});
+
+const storage = new multer.memoryStorage();
+
+async function imageUploadUtil(file) {
+  const result = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+
+  return result;
+}
+
+const upload = multer({  storage: multer.memoryStorage(),
+  limits: { fileSize: 15 * 1024 * 1024 }
+ });
+
+module.exports = { upload, imageUploadUtil };
